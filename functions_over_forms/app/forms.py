e6db70b22a7dcabb67bf4fd7e3decd4fc140f5fa
Jason woodruff <jasonwoodruff619@gmail.com>
2024-11-15 11:31:18 -0600

    Update forms.py

--- functions_over_forms/app/forms.py
+++ functions_over_forms/app/forms.py
@@ -5,20 +5,19 @@ class FontTimes(forms.Form):
     integer_input = forms.IntegerField()
 
 class NoTeenSum(forms.Form):
-    integer_1 = forms.IntegerField()
-    integer_2 = forms.IntegerField()
-    integer_3 = forms.IntegerField()
+    int_1 = forms.IntegerField()
+    int_2 = forms.IntegerField()
+    int_3 = forms.IntegerField()
 
 class XyzThere(forms.Form):
     given_string = forms.CharField()
 
 class CenteredAverage(forms.Form):
-    given_amount_1 = forms.IntegerField()
-    given_amount_2 = forms.IntegerField()
-    given_amount_3 = forms.IntegerField()
-    given_amount_4 = forms.IntegerField()
-    given_amount_5 = forms.IntegerField()
-    given_amount_6 = forms.IntegerField(required=False)
-    given_amount_7 = forms.IntegerField(required=False)
-
+    amount_1 = forms.IntegerField()
+    amount_2 = forms.IntegerField()
+    amount_3 = forms.IntegerField()
+    amount_4 = forms.IntegerField()
+    amount_5 = forms.IntegerField()
+    amount_6 = forms.IntegerField(required=False)
+    amount_7 = forms.IntegerField(required=False)
 
